526f3b2687a742e7c3271e8d7bdc7a24b19112bf
MichaelWebHub <michael.kutateladze@yandex.ru>
2020-01-10 16:41:01 +0300

    3.3.6 Fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-import { IUseReactiveForm, useReactiveForm } from './useReactiveForm';
+import { useReactiveForm } from './useReactiveForm';
 
-export {
-  useReactiveForm,
-  IUseReactiveForm
-};
+/** In interface of input values */
+export interface IUseReactiveForm<T> {
+  /** Form fields / structure */
+  fields: T;
+  /** If form is rendered dynamically, we need to pass a flag. True is set by default */
+  visible?: boolean;
+  /** Validation schema */
+  schema?: any;
+  /** Separator for name property of inputs. _ is set by default */
+  separator?: string;
+  /** Validate input on change */
+  validateOnChange?: boolean;
+  /** Action on change */
+  actionOnChange?: (values: T) => void;
+}
+
+export default useReactiveForm;
--- src/useReactiveForm.ts
+++ src/useReactiveForm.ts
@@ -2,34 +2,19 @@ import { Ref, useEffect, useRef, useState } from 'react';
 import { BehaviorSubject, fromEvent, Subject } from 'rxjs';
 import { debounceTime, map, takeUntil } from 'rxjs/operators';
 import { ValidationError } from 'yup';
-
-/** In interface of input values */
-export interface IUseReactiveForm<T> {
-  /** Form fields / structure */
-  fields: T;
-  /** If form is rendered dynamically, we need to pass a flag. True is set by default */
-  visible?: boolean;
-  /** Validation schema */
-  schema?: any;
-  /** Separator for name property of inputs. _ is set by default */
-  separator?: string;
-  /** Validate input on change */
-  validateOnChange?: boolean;
-  /** Action on change */
-  actionOnChange?: (values: T) => void;
-}
+import { IUseReactiveForm } from './index';
 
 /** Validation object */
-interface IValidationResult {
+export interface IValidationResult {
   value: string;
   error: string;
 }
 
 /** Тип элемента */
-type IField = HTMLInputElement | HTMLTextAreaElement;
+export type IField = HTMLInputElement | HTMLTextAreaElement;
 
 /** Interface of return values */
-interface IUseFormResult<T> {
+export interface IUseFormResult<T> {
   values: () => T;
   ref: Ref<HTMLFormElement>;
   update: (f: T) => void;
